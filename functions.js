710e62753017647561dd68bca3e2203521c92255
NatalieCastien <natalie.castien@gmail.com>
2020-06-01 16:04:16 +0200

    win function refactored and queryselectors within variables

--- functions.js
+++ functions.js
@@ -8,8 +8,7 @@ const functions = {
   isLetterIncluded: (word, letter) => word.includes(letter),
   isWordGuessed: (word, inputs) => {
     let remaining = word.filter((letter) => !inputs.includes(letter));
-    /* If the remaining array of letters is 0, then the word is guessed 
-    and it will return true, if not it returns false */
+    // If the remaining array of letters is 0, then the word is guessed
     return remaining.length === 0;
   },
   randomWord: () => wordList[Math.floor(Math.random() * wordList.length)],
